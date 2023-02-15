0aad27c9d9e87df7903eeb4e0a13f8ebcb6d6260
Carolina Ricagno <carom2896@gmail.com>
2023-02-15 02:28:56 -0300

    Commit inicial

---
+++ SBI_API/Controllers/SBI_Test.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using MediatR;
+using SBI_API.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http.Features;
+using System;
+
+namespace SBI_API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SBI_Test : ControllerBase
+    {
+        private readonly MediatR.IMediator _mediator;
+
+        public SBI_Test(MediatR.IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        // GET api/<SBI_Test>/5
+        [HttpGet]
+        public async Task<Salida> Get([FromQuery]int id)
+        {
+            //Task<List<Models.ServerPost>>
+            Salida salida = null;
+            try
+            {
+                List<ServerPost> posts = await this._mediator.Send(new Models.Request());
+
+                if (posts.Count > 0)
+                {
+                    ServerPost post = posts.Find(p => p.id == id);
+                    salida = post.getSalida(post);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                await Task.Run(() => throw new Exception(ex.ToString()));
+            }
+            return await Task<Salida>.Factory.StartNew(() => salida);
+        }
+
+        // POST api/<SBI_Test>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SBI_Test>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SBI_Test>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SBI_API/Models/Entities.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SBI_API.Models
+{
+    public class ServerPost
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        public ServerPost() { }
+
+        public Salida getSalida(ServerPost post)
+        {
+            Mapper mapper = new Mapper(this.setMapper());
+            return mapper.Map<ServerPost, Salida>(post);
+        }
+
+        private MapperConfiguration setMapper()
+        {
+            return new MapperConfiguration(c =>
+            {
+                c.CreateMap<ServerPost, Salida>()
+                .ForMember(salida => salida.Id, server => server.MapFrom(s => s.id));
+
+                c.CreateMap<ServerPost, Salida>()
+                .ForMember(salida => salida.Titulo, server => server.MapFrom(s => s.title));
+            });
+        }
+    }
+
+    public class Salida
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+    }
+
+    public interface IServicio
+    {
+        List<ServerPost> deserializarJSON();
+    }
+
+    public class Request: MediatR.IRequest<List<ServerPost>> { }
+
+    public class RequestHandler: MediatR.IRequestHandler<Request, List<ServerPost>> 
+    {
+        private readonly IServicio _servicio;
+        public RequestHandler(IServicio servicio)
+        {
+            _servicio = servicio;
+        }
+        public Task<List<ServerPost>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_servicio.deserializarJSON());
+        }
+    }
+
+    public class Servicio: IServicio
+    {
+        public List<ServerPost> deserializarJSON()
+        {
+            string json = new WebClient().DownloadString("https://jsonplaceholder.typicode.com/posts");
+            List<ServerPost> posts = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ServerPost>>(json);
+            return posts;
+        }
+    }
+
+
+}
